b63f2331a439732421db7123011edee96e3be803
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2018-11-02 09:38:38 +0100

    get needed variables from API

--- index.js
+++ index.js
@@ -23,41 +23,77 @@ client
     {
       q: "format:book",
       librarian: true,
-      refine: true
+      refine: true,
+      facet: "language(eng)"
     },
     "title"
   )
-  .then(response => JSON.parse(response).aquabrowser.results.result)
+  // .get(
+  //   "refine",
+  //   {
+  //   rctx:
+  //     "ARXIMQ6CMBQG4P@REEI4ggtuDiYIOnkZ08ILNJYWCk2ME5Mn8CZuHsSbuIvLN3wEkSFB6Ss$1Te$d6M1U9m116PqGuUqQUh7qS0EUuV47ZV9HvTcsvKsm7zn$fxPEht98VxPA@tBthxXhwifFy1mMdH7K2j3fBABiVMSwLbwzs2F0WPQTbFeErxBbA39AA==",
+  //   count: 100
+  // })
+
+  .then(response => JSON.parse(response).aquabrowser)
   .then(response => {
     //console.log(response);
-    var keys = getKeys(response);
-    var importTime = getImortTime(response);
-    var languages = getLanguages(response);
-    var subjects = getSubjects(response);
+    var result = response.results.result;
+    console.log(result);
+    var keys = getKeys(result);
+    var format = getFormat(result);
+    var languages = getLanguages(result);
+    var genres = getGenres(result);
+
+    var facet = response.facets;
+    console.log(facet);
   })
+
   .catch(err => console.log(err)); // Something went wrong in the request to the API
 
 function getKeys(data) {
   //console.log(data);
   var keys = Object.keys(data[0]);
-  console.log("Keys: ", keys);
+  //console.log("Keys: ", keys);
   return keys;
 }
 
-function getImortTime(data) {
+function getFormat(data) {
   data.forEach(function(data) {
-    console.log(data["librarian-info"].info["import-time"]);
+    //console.log(data.formats.format["search-term"]);
   });
 }
 
 function getLanguages(data) {
   data.forEach(function(data) {
-    console.log(data.languages.language["search-term"]);
+    //console.log(data.languages.language["search-term"]);
   });
 }
 
-function getSubjects(data) {
+function getGenres(data) {
   data.forEach(function(data) {
-    console.log(data.subjects["topical-subject"]["search-term"]);
+    //console.log(data.genres.genre["search-term"]);
+
+    var genres = data.genres;
+
+    if (genres == undefined) {
+      console.log("there is no genre");
+    } else {
+      var genre = genres.genre;
+      var genreSearchTerm = genre["search-term"];
+      //console.log(genre);
+      console.log(genreSearchTerm);
+      console.log(typeof genre);
+
+      if (genreSearchTerm == undefined) {
+        console.log("Dit boek heeft meer dan 1 genre:");
+        genre.forEach(function(genre) {
+          console.log("genre : " + genre["search-term"]);
+        });
+      } else {
+        //console.log("error");
+      }
+    }
   });
 }
