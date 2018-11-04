3ce961cb4817de09105482bd43921bec3075bf4b
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2018-11-04 18:41:10 +0100

    added second .get request for working with REFINE

--- index.js
+++ index.js
@@ -12,88 +12,60 @@ const client = new OBA({
 // General usage:
 // client.get({ENDPOINT}, {PARAMS});
 // ENDPOINT = search | details | refine | schema | availability | holdings
-// PARAMS = API url parameter options (see api docs for more info)
+// PARAMS = API url parameter options (see api docs for more info) https://zoeken.oba.nl/api/v1/
 
 // Client returns a promise which resolves the APIs output in JSON
 
-// Example search to the word 'rijk' sorted by title:
+var rctx;
+
 client
   .get(
     "search",
     {
       q: "format:book",
       librarian: true,
-      refine: true,
-      facet: "language(eng)"
+      refine: true
+      //facet: "language(eng)"
     },
     "title"
   )
-  // .get(
-  //   "refine",
-  //   {
-  //   rctx:
-  //     "ARXIMQ6CMBQG4P@REEI4ggtuDiYIOnkZ08ILNJYWCk2ME5Mn8CZuHsSbuIvLN3wEkSFB6Ss$1Te$d6M1U9m116PqGuUqQUh7qS0EUuV47ZV9HvTcsvKsm7zn$fxPEht98VxPA@tBthxXhwifFy1mMdH7K2j3fBABiVMSwLbwzs2F0WPQTbFeErxBbA39AA==",
-  //   count: 100
-  // })
-
   .then(response => JSON.parse(response).aquabrowser)
   .then(response => {
     //console.log(response);
-    var result = response.results.result;
-    console.log(result);
-    var keys = getKeys(result);
-    var format = getFormat(result);
-    var languages = getLanguages(result);
-    var genres = getGenres(result);
-
-    var facet = response.facets;
-    console.log(facet);
+    var rctx = response.meta.rctx;
+    console.log(rctx);
+    return rctx;
   })
-
+  .then(rctx => {
+    client
+      .get("refine", {
+        rctx: rctx,
+        count: 100
+      })
+      .then(rctx => JSON.parse(rctx).aquabrowser)
+      .then(rctx => {
+        var originalQuery = rctx.meta["original-query"];
+        // console.log(originalQuery);
+        var facet = rctx.facets.facet;
+        var languageFacet = getLanguageFacet(facet);
+        var facetDate = getFacetData(facet);
+      });
+  })
   .catch(err => console.log(err)); // Something went wrong in the request to the API
 
-function getKeys(data) {
-  //console.log(data);
-  var keys = Object.keys(data[0]);
-  //console.log("Keys: ", keys);
-  return keys;
-}
-
-function getFormat(data) {
-  data.forEach(function(data) {
-    //console.log(data.formats.format["search-term"]);
-  });
-}
-
-function getLanguages(data) {
-  data.forEach(function(data) {
-    //console.log(data.languages.language["search-term"]);
-  });
-}
-
-function getGenres(data) {
-  data.forEach(function(data) {
-    //console.log(data.genres.genre["search-term"]);
-
-    var genres = data.genres;
-
-    if (genres == undefined) {
-      console.log("there is no genre");
-    } else {
-      var genre = genres.genre;
-      var genreSearchTerm = genre["search-term"];
-      //console.log(genre);
-      console.log(genreSearchTerm);
-      console.log(typeof genre);
-
-      if (genreSearchTerm == undefined) {
-        console.log("Dit boek heeft meer dan 1 genre:");
-        genre.forEach(function(genre) {
-          console.log("genre : " + genre["search-term"]);
-        });
-      } else {
-        //console.log("error");
-      }
+function getLanguageFacet(facetData) {
+  facetData.forEach(function(facetData) {
+    var facetId = facetData.id;
+    if (facetId === "Genre") {
+      console.log(facetData);
     }
   });
+}
+
+function getFacetData(data) {
+  console.log("getFacetData");
+}
+
+function getRctx(data) {
+  console.log("getRctx");
 }
