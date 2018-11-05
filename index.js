8d9f42236109161f26b593efdddd11bf8728548a
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2018-11-05 20:26:49 +0100

    can get rctx from facet(language(language))

--- index.js
+++ index.js
@@ -17,26 +17,29 @@ const client = new OBA({
 // Client returns a promise which resolves the APIs output in JSON
 
 var rctx;
+var selectedValues = [];
+var selectedFacetId = [];
 
 client
   .get(
+    // vraag alle boeken op
     "search",
     {
       q: "format:book",
       librarian: true,
       refine: true
-      //facet: "language(eng)"
     },
     "title"
   )
   .then(response => JSON.parse(response).aquabrowser)
   .then(response => {
-    //console.log(response);
+    // neem rctx
     var rctx = response.meta.rctx;
-    console.log(rctx);
     return rctx;
   })
+
   .then(rctx => {
+    // kijk in de rctx naar het totaal aantal boeken per Genre
     client
       .get("refine", {
         rctx: rctx,
@@ -44,28 +47,58 @@ client
       })
       .then(rctx => JSON.parse(rctx).aquabrowser)
       .then(rctx => {
-        var originalQuery = rctx.meta["original-query"];
-        // console.log(originalQuery);
-        var facet = rctx.facets.facet;
-        var languageFacet = getLanguageFacet(facet);
-        var facetDate = getFacetData(facet);
+        var allFacets = rctx.facets.facet;
+        var languageFacet = getLanguageFacet(allFacets);
+        return selectedFacetId;
+      })
+
+      .then(rctx => {
+        var selectedFacetIds = [
+          "language(ger)",
+          "language(eng)",
+          "language(fre)"
+        ]; // vervangen met automatisch gegenereerde Array
+        selectedFacetIds.forEach(function(selectedFacetIds) {
+          client
+            .get("search", {
+              q: "format:book",
+              facet: selectedFacetIds,
+              refine: true
+            })
+            .then(res => JSON.parse(res).aquabrowser)
+            .then(res => {
+              console.log("RCTX per taal");
+              console.log(res.meta.rctx);
+            });
+        });
       });
   })
   .catch(err => console.log(err)); // Something went wrong in the request to the API
 
+//!!!!! FUNCTIONS
+
 function getLanguageFacet(facetData) {
-  facetData.forEach(function(facetData) {
-    var facetId = facetData.id;
-    if (facetId === "Genre") {
-      console.log(facetData);
-    }
-  });
-}
-
-function getFacetData(data) {
-  console.log("getFacetData");
-}
-
-function getRctx(data) {
-  console.log("getRctx");
+  // facetData.forEach(function(facetData) {
+  //   var facetKey = facetData.id;
+  //
+  //   if (facetKey === "Language") {
+  //     var allLanguages = facetData.value;
+  //     allLanguages.forEach(function(allLanguages) {
+  //       var count = parseInt(allLanguages.count);
+  //       var id = allLanguages.id;
+  //       var facetId = "language(" + id + ")";
+  //
+  //       if (count > 20000) {
+  //         // als aantal groter is dan ... sla data op in:
+  //         selectedValues.push({
+  //           languageId: id,
+  //           languageCount: count
+  //         });
+  //         selectedFacetId.push({
+  //           facetId
+  //         });
+  //       }
+  //     });
+  //   }
+  // });
 }
